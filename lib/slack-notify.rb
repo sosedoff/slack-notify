ece86f97e91cb0af9011f4d944f314d4d0e007f1
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-04-18 21:57:22 -0500

    Extract network features into connection module

--- lib/slack-notify.rb
+++ lib/slack-notify.rb
@@ -1,5 +1,6 @@
 require "slack-notify/version"
 require "slack-notify/error"
+require "slack-notify/connection"
 require "slack-notify/payload"
 
 require "json"
@@ -7,6 +8,8 @@ require "faraday"
 
 module SlackNotify
   class Client
+    include SlackNotify::Connection
+
     def initialize(team, token, options = {})
       @team     = team
       @token    = token
@@ -50,37 +53,6 @@ module SlackNotify
 
     def delivery_channels(channel)
       [channel || @channel || "#general"].flatten.compact.uniq
-    end
-
-    def send_payload(payload)
-      conn = Faraday.new(hook_url, { timeout: 5, open_timeout: 5 }) do |c|
-        c.use(Faraday::Request::UrlEncoded)
-        c.adapter(Faraday.default_adapter)
-      end
-
-      response = conn.post do |req|
-        req.body = JSON.dump(payload.to_hash)
-      end
-
-      handle_response(response)
-    end
-
-    def handle_response(response)
-      unless response.success?
-        if response.body.include?("\n")
-          raise SlackNotify::Error
-        else
-          raise SlackNotify::Error.new(response.body)
-        end
-      end
-    end
-
-    def hook_url
-      "#{base_url}/services/hooks/incoming-webhook?token=#{@token}"
-    end
-
-    def base_url
-      "https://#{@team}.slack.com"
     end
   end
 end
