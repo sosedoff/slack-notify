20d86d8ad25a6bb0e386cbac5f2773ae87a00824
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-04-18 22:43:07 -0500

    Extract client code into separate file

---
+++ lib/slack-notify/client.rb
@@ -0,0 +1,55 @@
+require "json"
+require "faraday"
+
+module SlackNotify
+  class Client
+    include SlackNotify::Connection
+
+    def initialize(team, token, options = {})
+      @team       = team
+      @token      = token
+      @username   = options[:username]
+      @channel    = options[:channel]
+      @icon_url   = options[:icon_url]
+      @icon_emoji = options[:icon_emoji]
+
+      validate_arguments
+    end
+
+    def test
+      notify("Test Message")
+    end
+
+    def notify(text, channel = nil)
+      delivery_channels(channel).each do |chan|
+        payload = SlackNotify::Payload.new(
+          text:       text,
+          channel:    chan,
+          username:   @username,
+          icon_url:   @icon_url,
+          icon_emoji: @icon_emoji
+        )
+
+        send_payload(payload)
+      end
+
+      true
+    end
+
+    private
+
+    def validate_arguments
+      raise ArgumentError, "Team name required" if @team.nil?
+      raise ArgumentError, "Token required"     if @token.nil?
+      raise ArgumentError, "Invalid team name"  unless valid_team_name?
+    end
+
+    def valid_team_name?
+      @team =~ /^[a-z\d\-]+$/ ? true : false
+    end
+
+    def delivery_channels(channel)
+      [channel || @channel || "#general"].flatten.compact.uniq
+    end
+  end
+end
