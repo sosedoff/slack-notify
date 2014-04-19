db10c6035f6059fd23e3c538003374c15ca4a218
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-04-18 21:57:32 -0500

    Add missing connection file

---
+++ lib/slack-notify/connection.rb
@@ -0,0 +1,34 @@
+module SlackNotify
+  module Connection
+    def send_payload(payload)
+      conn = Faraday.new(hook_url, { timeout: 5, open_timeout: 5 }) do |c|
+        c.use(Faraday::Request::UrlEncoded)
+        c.adapter(Faraday.default_adapter)
+      end
+
+      response = conn.post do |req|
+        req.body = JSON.dump(payload.to_hash)
+      end
+
+      handle_response(response)
+    end
+
+    def handle_response(response)
+      unless response.success?
+        if response.body.include?("\n")
+          raise SlackNotify::Error
+        else
+          raise SlackNotify::Error.new(response.body)
+        end
+      end
+    end
+
+    def hook_url
+      "#{base_url}/services/hooks/incoming-webhook?token=#{@token}"
+    end
+
+    def base_url
+      "https://#{@team}.slack.com"
+    end
+  end
+end
