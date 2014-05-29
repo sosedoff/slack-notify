90cb8e17d169057068a51cfeebc1cffc5ff300c5
Matthieu Aussaguel <matthieu.aussaguel@gmail.com>
2014-05-29 21:18:40 +0700

    Upgrade Faraday to 0.9.0

--- lib/slack-notify/connection.rb
+++ lib/slack-notify/connection.rb
@@ -1,13 +1,15 @@
 module SlackNotify
   module Connection
     def send_payload(payload)
-      conn = Faraday.new(hook_url, { timeout: 5, open_timeout: 5 }) do |c|
+      conn = Faraday.new(hook_url) do |c|
         c.use(Faraday::Request::UrlEncoded)
         c.adapter(Faraday.default_adapter)
+        c.options.timeout      = 5
+        c.options.open_timeout = 5
       end
 
       response = conn.post do |req|
-        req.body = JSON.dump(payload.to_hash)
+        req.body                 = JSON.dump(payload.to_hash)
       end
 
       handle_response(response)
